22ce07f73f63bf81289adeb077a619d74934e782
meteora277 <justin.s.diaz@gmail.com>
2021-12-06 18:25:54 -0500

    added route for new url submissions

--- express_server.js
+++ express_server.js
@@ -12,6 +12,9 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render('urls_show', templateVars);
@@ -24,6 +27,7 @@ app.get('/urls', (req, res) => {
 app.get('/urls.json', (req ,res) => {
   res.json(urlDatabase);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
