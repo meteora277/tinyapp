c8610b10c38efb9d85b930e793fd474a8f2c5f88
meteora277 <justin.s.diaz@gmail.com>
2021-12-08 11:14:24 -0500

    updated all template vars to pass in user object instead of just username

--- express_server.js
+++ express_server.js
@@ -44,19 +44,24 @@ app.get('/', (req, res) => {
 
 //renders page for creating a new key: value in the db
 app.get('/urls/new', (req, res) => {
-  const templateVars = {username: req.cookies["username"]};
-  
+
+  const user = users[req.cookies["user_id"]];
+  const templateVars = {user: user};
   res.render('urls_new', templateVars);
+
 });
 
 app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]};
+  const user = users[req.cookies["user_id"]];
+  
+  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], user: user};
   res.render('urls_show', templateVars);
 });
 
 //renders all the urls into a template
 app.get('/urls', (req, res) => {
-  const templateVars = {urls: urlDatabase, username: req.cookies["username"]};
+  const user = users[req.cookies["user_id"]] || undefined;
+  const templateVars = {urls: urlDatabase, user: user};
   res.render('urls_index', templateVars);
 });
 app.get('/urls.json', (req ,res) => {
@@ -65,7 +70,7 @@ app.get('/urls.json', (req ,res) => {
 
 // if shortUrl key is in the database it will redirect, else return to /urls page
 app.get('/u/:shortURL', (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL];
+  const longURL = urlDatabase[req.params.shortURL];
   if (longURL === undefined) {
     res.redirect('/urls');
   }
@@ -73,8 +78,8 @@ app.get('/u/:shortURL', (req, res) => {
    
 });
 app.get('/register', (req, res) => {
-
-  let templateVars = {username: req.cookies["username"]};
+  const user = users[req.cookies["user_id"]];
+  const templateVars = {user: user};
   res.render('urls_register', templateVars);
 
 });
@@ -83,7 +88,7 @@ app.get('/register', (req, res) => {
 //it will also check if link start with http:// becuase redirect doesn't seem to work without it.
 app.post('/urls', (req, res) => {
 
-  let key = generateRandomString();
+  const key = generateRandomString();
   if (req.body.longURL.slice(0,7) !== 'http://') {
     urlDatabase[key] = 'http://' + req.body.longURL;
   } else {
@@ -99,6 +104,7 @@ app.post('/urls/:shortURL/delete', (req, res) =>{
   res.redirect('/urls');
 
 });
+
 //will update key in the db based on post wildcard
 app.post('/urls/:shortURL/update', (req, res) => {
   let shortUrl = req.params.shortURL;
@@ -113,14 +119,17 @@ app.post('/login', (req, res) => {
   res.cookie('username', req.body.username, { expires: new Date(Date.now() + 900000)});
   res.redirect('/urls');
 });
+
+
 //clears cookies when user presses logout button
 app.post('/logout', (req, res) =>{
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect('/urls');
 });
+
+
 //grabs form info from /register and add a new user to the users db
 app.post('/register', (req, res) => {
-  console.log(req.body);
   const salt = "IamSalt";
   const hash = createHash('sha256').update(salt + req.body.password).digest('binary');
   const id = generateRandomString();
